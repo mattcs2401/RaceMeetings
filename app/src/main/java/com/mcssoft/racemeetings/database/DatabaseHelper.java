7e47d480fb045f42574a507452a447565ab12a03
mattcs2401 <mattcs2401@gmail.com>
2017-02-16 16:57:28 +1000

    Updates for database helper and utility.

--- app/src/main/java/com/mcssoft/racemeetings/database/DatabaseHelper.java
+++ app/src/main/java/com/mcssoft/racemeetings/database/DatabaseHelper.java
@@ -20,6 +20,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context) {
         super(context, SchemaConstants.DATABASE_NAME, null, SchemaConstants.DATABASE_VERSION);
+        db = this.getWritableDatabase();
     }
 
     @Override
@@ -45,6 +46,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + SchemaConstants.DATABASE_NAME + "." + SchemaConstants.CLUBS_TABLE + ";");
     }
 
+    public SQLiteDatabase getDatabase() {
+        return db;
+    }
+
     public enum Projection {
         RegionSchema, ClubSchema
     }
@@ -57,78 +62,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 return getClubProjection();
         }
         return  null;
-    }
-
-    /**
-     * Utility method to see if rows exist in the given table.
-     * @param tableName The table to check.
-     * @return True if the row count > 0.
-     */
-    public boolean checkTableRowCount(String tableName) {
-        String[] projection = {};
-        if(tableName == SchemaConstants.REGIONS_TABLE) {
-            projection = getProjection(Projection.RegionSchema);
-        } else if(tableName == SchemaConstants.CLUBS_TABLE) {
-            projection = getProjection(Projection.ClubSchema);
-        }
-        SQLiteDatabase db = getWritableDatabase();
-        db.beginTransaction();
-        Cursor cursor = db.query(tableName, projection, null, null, null, null, null);
-        db.endTransaction();
-        return (cursor.getCount() > 0);
-    }
-
-    public void insertFromList(String tableName, ArrayList theList) {
-        if(tableName.equals(SchemaConstants.REGIONS_TABLE)) {
-            insertFromListRegions(theList);
-        } else if (tableName.equals(SchemaConstants.CLUBS_TABLE)) {
-            insertFromListClubs(theList);
-        }
-    }
-
-    private void insertFromListRegions(ArrayList theList) {
-        ContentValues cv;
-        SQLiteDatabase db = getWritableDatabase();
-
-        for (Object object : theList) {
-            Region region = (Region) object;
-            cv = new ContentValues();
-            cv.put(SchemaConstants.REGIONS_ID, region.getRegionId());
-            cv.put(SchemaConstants.REGIONS_NAME, region.getRegionName());
-            cv.put(SchemaConstants.REGIONS_S_NAME, region.getRegionSName());
-
-            try {
-                db.beginTransaction();
-                db.insertOrThrow(SchemaConstants.REGIONS_TABLE, null, cv);
-                db.setTransactionSuccessful();
-            } catch(SQLException ex) {
-                ex.printStackTrace();
-            } finally {
-                db.endTransaction();
-            }
-        }
-    }
-
-    private void insertFromListClubs(ArrayList theList) {
-        ContentValues cv;
-        SQLiteDatabase db = getWritableDatabase();
-
-        for (Object object : theList) {
-            Club club = (Club) object;
-            cv = new ContentValues();
-            cv.put(SchemaConstants.CLUB_ID, club.getClubId());
-            cv.put(SchemaConstants.CLUB_NAME, club.getClubName());
-
-            try {
-                db.beginTransaction();
-                db.insertOrThrow(SchemaConstants.CLUBS_TABLE, null, cv);
-                db.setTransactionSuccessful();
-            } catch(SQLException ex) {
-                ex.printStackTrace();
-            } finally {
-                db.endTransaction();
-            }
-        }
     }
 
     private static final String[] getRegionProjection() {
--- app/src/main/java/com/mcssoft/racemeetings/utility/DatabaseUtility.java
+++ app/src/main/java/com/mcssoft/racemeetings/utility/DatabaseUtility.java
@@ -1,6 +1,10 @@
 package com.mcssoft.racemeetings.utility;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 import java.io.ByteArrayInputStream;
@@ -23,23 +27,15 @@ public class DatabaseUtility implements IAsyncResponse {
 
     public DatabaseUtility(Context context) {
         this.context = context;
+        databaseHelper = new DatabaseHelper(context);
     }
 
     /**
      * Sanity check that database base tables have values (rows data).
      */
     public void databaseCheck() {
-
-        databaseHelper = new DatabaseHelper(context);
-
-        if(!databaseHelper.checkTableRowCount(SchemaConstants.REGIONS_TABLE)) {
+        if(!checkTableRowCount(SchemaConstants.REGIONS_TABLE)) {
             loadRegionsTableData();
-        }
-
-        // Note: this won't be called if there is no REGIONS data as the async task returns to
-        // method processFinished().
-        if(!databaseHelper.checkTableRowCount(SchemaConstants.CLUBS_TABLE)) {
-            loadClubsTableData();
         }
     }
 
@@ -55,25 +51,95 @@ public class DatabaseUtility implements IAsyncResponse {
 
         if(regions) {
             ArrayList<Region> regions = mxmlp.parseRegionsXml();
-            databaseHelper.insertFromList(SchemaConstants.REGIONS_TABLE, regions);
+            insertFromList(SchemaConstants.REGIONS_TABLE, regions);
 
             // If REGIONS data doesn't exist then it's likely CLUBS data won't exist either.
-            if(!databaseHelper.checkTableRowCount(SchemaConstants.CLUBS_TABLE)) {
-                theResults = "";
+            if(!checkTableRowCount(SchemaConstants.CLUBS_TABLE)) {
                 loadClubsTableData();
             }
         } else if (clubs) {
             ArrayList<Club> clubs = mxmlp.parseClubsXml();
-            databaseHelper.insertFromList(SchemaConstants.CLUBS_TABLE, clubs);
+            insertFromList(SchemaConstants.CLUBS_TABLE, clubs);
+        }
+    }
+
+    /**
+     * Utility method to see if rows exist in the given table.
+     * @param tableName The table to check.
+     * @return True if the row count > 0.
+     */
+    public boolean checkTableRowCount(String tableName) {
+        String[] projection = {};
+        if(tableName == SchemaConstants.REGIONS_TABLE) {
+            projection = databaseHelper.getProjection(DatabaseHelper.Projection.RegionSchema);
+        } else if(tableName == SchemaConstants.CLUBS_TABLE) {
+            projection = databaseHelper.getProjection(DatabaseHelper.Projection.ClubSchema);
+        }
+        SQLiteDatabase db = databaseHelper.getDatabase();
+        db.beginTransaction();
+        Cursor cursor = db.query(tableName, projection, null, null, null, null, null);
+        db.endTransaction();
+        return (cursor.getCount() > 0);
+    }
+
+    public void insertFromList(String tableName, ArrayList theList) {
+        if(tableName.equals(SchemaConstants.REGIONS_TABLE)) {
+            insertFromListRegions(theList);
+        } else if (tableName.equals(SchemaConstants.CLUBS_TABLE)) {
+            insertFromListClubs(theList);
+        }
+    }
+
+    private void insertFromListRegions(ArrayList theList) {
+        ContentValues cv;
+        SQLiteDatabase db = databaseHelper.getDatabase();
+
+        for (Object object : theList) {
+            Region region = (Region) object;
+            cv = new ContentValues();
+            cv.put(SchemaConstants.REGIONS_ID, region.getRegionId());
+            cv.put(SchemaConstants.REGIONS_NAME, region.getRegionName());
+            cv.put(SchemaConstants.REGIONS_S_NAME, region.getRegionSName());
+
+            try {
+                db.beginTransaction();
+                db.insertOrThrow(SchemaConstants.REGIONS_TABLE, null, cv);
+                db.setTransactionSuccessful();
+            } catch(SQLException ex) {
+                ex.printStackTrace();
+            } finally {
+                db.endTransaction();
+            }
+        }
+    }
+
+    private void insertFromListClubs(ArrayList theList) {
+        ContentValues cv;
+        SQLiteDatabase db = databaseHelper.getDatabase();
+
+        for (Object object : theList) {
+            Club club = (Club) object;
+            cv = new ContentValues();
+            cv.put(SchemaConstants.CLUB_ID, club.getClubId());
+            cv.put(SchemaConstants.CLUB_NAME, club.getClubName());
+
+            try {
+                db.beginTransaction();
+                db.insertOrThrow(SchemaConstants.CLUBS_TABLE, null, cv);
+                db.setTransactionSuccessful();
+            } catch(SQLException ex) {
+                ex.printStackTrace();
+            } finally {
+                db.endTransaction();
+            }
         }
     }
 
     private void loadRegionsTableData() {
         regions = true; clubs = false;
         try {
-            DownloadData dld = new DownloadData(context);
+            DownloadData dld = new DownloadData(context, new URL(createRegionsUrl()));
             dld.asyncResponse = this;
-            dld.setUrl(new URL(createRegionsUrl()));
             dld.execute();
         } catch(Exception ex) {
             String s = ex.getMessage();
@@ -83,9 +149,8 @@ public class DatabaseUtility implements IAsyncResponse {
     private void loadClubsTableData() {
         regions = false; clubs = true;
         try {
-            DownloadData dld = new DownloadData(context);
+            DownloadData dld = new DownloadData(context, new URL(createClubsUrl()));
             dld.asyncResponse = this;
-            dld.setUrl(new URL(createClubsUrl()));
             dld.execute();
         } catch(Exception ex) {
             String s = ex.getMessage();
