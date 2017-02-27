2bf32f68f0675613fa83c69b2259d4d7e65ad8ab
mattcs2401 <mattcs2401@gmail.com>
2017-02-27 14:55:27 +1000

    Initial skeleton for preference change listener.

--- app/src/main/java/com/mcssoft/racemeetings/activity/SettingsActivity.java
+++ app/src/main/java/com/mcssoft/racemeetings/activity/SettingsActivity.java
@@ -1,13 +1,16 @@
 package com.mcssoft.racemeetings.activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 import com.mcssoft.racemeetings.R;
 import com.mcssoft.racemeetings.fragment.SettingsFragment;
 
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedState) {
@@ -16,5 +19,26 @@ public class SettingsActivity extends PreferenceActivity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+//        if (key.equals(SOME_KEY)) {
+//            // do something.
+//        }
     }
 }
